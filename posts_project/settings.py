75deb3d472254297947799782ce889e088cb94d6
Ar1FD <arifarhansyachdiraja@students.unnes.ac.id>
2024-03-15 14:56:26 +0700

    adding like and unlike feature

--- posts_project/settings.py
+++ posts_project/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'posts.middleware.AjaxMiddleware', 
 ]
 
 ROOT_URLCONF = 'posts_project.urls'
@@ -131,6 +132,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
