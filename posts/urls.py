9c61810892be318b5e56fca1ce61b7d8ecbeffc3
Ar1FD <arifarhansyachdiraja@students.unnes.ac.id>
2024-03-25 13:21:35 +0700

    posts detail page finished

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
-
+    post_detail_data_view,
     hello_world_view
 )
 
@@ -15,7 +15,6 @@ urlpatterns = [
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
-
-
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
     path('hello-world/', hello_world_view, name='hello-world'),
 ]
